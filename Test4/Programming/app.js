bdf322bcb82bde51b5ff79a04622f1176e23aedc
Noothi Srivarshini <srivarshini587@gmail.com>
2026-06-27 14:40:29 +0530

    updated with string reverse and even_odd

---
+++ Test4/Programming/app.js
@@ -0,0 +1,24 @@
+//Even or Odd
+
+
+let number = 3;
+
+if(number%2==0){
+    console.log("Even");
+}else{
+    console.log("Odd");
+}
+
+
+//String Reverse
+
+let name = "hello";
+
+for(let i=name.length-1;i>=0;i--){
+    console.log(name[i]);
+}
+
+
+
+
+
